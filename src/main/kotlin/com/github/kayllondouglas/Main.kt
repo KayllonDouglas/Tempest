1cfe991c8726a26706dc8940978797d2e31e0088
KayllonDouglas <kayllondouglas@gmail.com>
2022-06-20 18:06:25 -0300

    Fixing2

--- src/main/kotlin/com/github/kayllondouglas/Main.kt
+++ src/main/kotlin/com/github/kayllondouglas/Main.kt
@@ -5,7 +5,11 @@ import cn.nukkit.plugin.PluginBase
 class Main: PluginBase() {
   
   override fun onEnable() {
-    this.getLogger().info("§aTempestCore is now enabled, version [" + this.getDescription().getVersion() + "]") 
+    this.getLogger().info("§aTempestCore is now enabled, version [" + this.getDescription().getVersion() + "].") 
+  }
+
+  override fun onDisable() {
+    this.getLogger().info("§cTempestCore is now disabled.")
   }
 
 }
