405f4692d4a6f635c524cb261965587eb8743aa3
KayllonDouglas <kayllondouglas@gmail.com>
2022-06-21 10:57:51 -0300

    Testing Config

--- src/main/kotlin/com/github/kayllondouglas/Main.kt
+++ src/main/kotlin/com/github/kayllondouglas/Main.kt
@@ -1,12 +1,18 @@
 package com.github.kayllondouglas
 
+import cn.nukkit.utils.Config
 import cn.nukkit.plugin.PluginBase
+
+import java.io.File
 
 class Main : PluginBase() {
 
+  var configuration: Config? = null
+
   override fun onEnable() {
-    this.getLogger()
-        .info("§aTempestCore is now enabled, version [" + this.getDescription().getVersion() + "].")
+    this.getDataFolder().mkdirs()
+    configuration = Config(File(this.getDataFolder(), "Configuration.json"), Config.JSON)
+    this.getLogger().info("§aTempestCore is now enabled, version [" + this.getDescription().getVersion() + "].")
   }
 
   override fun onDisable() {
