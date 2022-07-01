abe5731c8d3071fdbffa966f81fa51d0990d1fb0
KayllonDouglas <kayllon.doug10@gmail.com>
2022-07-01 13:56:32 -0300

    Refactoring Factions System, again

--- src/main/kotlin/com/github/kayllondouglas/faction/Faction.kt
+++ src/main/kotlin/com/github/kayllondouglas/faction/Faction.kt
@@ -2,7 +2,7 @@ package com.github.kayllondouglas.faction
 
 import java.util.UUID
 
-data class Faction(
+data class Faction (
     val tag: String,
     val description: String,
     var members: HashMap<UUID, FactionMember>
